98f70942d829bb95515ee05bbd5de6a182844c22
blenddehari <blenddehari@gmail.com>
2017-11-13 18:31:22 +0100

    changed design layout; added the textView, EditTexts, Buttons

--- app/src/main/java/com/example/blend/cowcounter/MainActivity.java
+++ app/src/main/java/com/example/blend/cowcounter/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.blend.cowcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String LOGGING_TAG;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(LOGGING_TAG,getLocalClassName() + ".onCreate is called!");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button AddButton = (Button) findViewById(R.id.button_add);
+
+        EditText editText_breed = (EditText) findViewById(R.id.editText_breed);
+        editText_breed.setText("breed", TextView.BufferType.EDITABLE);
+
+        AddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(LOGGING_TAG, getLocalClassName() + "Goodbye!");
+
+            }
+        });
     }
 }
